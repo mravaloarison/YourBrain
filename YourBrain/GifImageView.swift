cf0eaa70d84a049c13a7b07a42f38345f3b75390
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-12-01 20:49:38 -0500

    added gif

---
+++ YourBrain/GifImageView.swift
@@ -0,0 +1,18 @@
+//
+//  GifImageView.swift
+//  YourBrain
+//
+//  Created by Mami Ravaloarison on 12/1/24.
+//
+
+import SwiftUI
+
+struct GifImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GifImageView()
+}
