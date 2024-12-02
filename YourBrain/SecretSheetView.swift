b6c5ea1e24909d89b172ac2aac5085f39ebfd58f
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-12-01 23:23:02 -0500

    commitas

---
+++ YourBrain/SecretSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SecretSheetView.swift
+//  YourBrain
+//
+//  Created by Mami Ravaloarison on 12/1/24.
+//
+
+import SwiftUI
+
+struct SecretSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecretSheetView()
+}
